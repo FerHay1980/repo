6087d34e92b7e8b2ecc6c3b93b254c9e6f2c77e7
I074102 <I074102@WDFN34205180A.emea.global.corp.sap>
2020-11-26 14:31:47 +0100

    Change1

--- Test_Repo/src/TestDates.java
+++ Test_Repo/src/TestDates.java
@@ -7,16 +7,7 @@ public class TestDates {
 
 	public static void main(String[] args) throws ParseException {
 		// TODO Auto-generated method stub
-		SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-        Date startDate = dateFormatter.parse("2020-08-04T13:36:00");
-        Date dateFrom = startDate;
-        Date dateTo = addMinutesToDate(dateFrom,2);
-        Date endDate = dateFormatter.parse("2020-08-04T13:50:00");
-
-        while(endDate.compareTo(dateTo) >= 0 ){
-        	System.out.println("From: " + dateFrom + " To: " +  dateTo);
-        	dateFrom.setTime(dateFrom.getTime() + 120000);
-        	dateTo.setTime(dateTo.getTime() + 120000);
+	
         	//Change Has been Applied
 		//Change Applied on Git side
 		// One more change from Git side
@@ -24,15 +15,12 @@ public class TestDates {
 		// Change made on Gitside
         	//BHP Demo change
 		//BHP Change from git side(mimic other users change)
+        	
+        	
+        	
         }
         
         
 	}
 	
-	public static Date addMinutesToDate(Date date, int minutes) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(date);
-        calendar.add(Calendar.MINUTE, minutes);
-        return calendar.getTime();
-    }
-}
+
